ce76f8ba3d88ec74d1f6cf395d69e42e061357b4
Christian Blankenberg <christian.blankenberg@seleon.de>
2023-12-29 19:17:54 +0100

    Increment, Remove Android Project

--- Android.3XO/3XO/MainActivity.cs
+++ Android.3XO/3XO/MainActivity.cs
@@ -11,26 +11,26 @@ namespace _3XO
 
         private void InitButtons()
         {
-            this.buttons[0, 0] = (Button)this.FindViewById(Resource.Id.button00);
-            this.buttons[0, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 0], 0, 0);
-            this.buttons[1, 0] = (Button)this.FindViewById(Resource.Id.button10);
-            this.buttons[1, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 0], 1, 0);
-            this.buttons[2, 0] = (Button)this.FindViewById(Resource.Id.button20);
-            this.buttons[2, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 0], 2, 0);
+            //this.buttons[0, 0] = (Button)this.FindViewById(Resource.Id.button00);
+            //this.buttons[0, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 0], 0, 0);
+            //this.buttons[1, 0] = (Button)this.FindViewById(Resource.Id.button10);
+            //this.buttons[1, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 0], 1, 0);
+            //this.buttons[2, 0] = (Button)this.FindViewById(Resource.Id.button20);
+            //this.buttons[2, 0].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 0], 2, 0);
 
-            this.buttons[0, 1] = (Button)this.FindViewById(Resource.Id.button01);
-            this.buttons[0, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 1], 0, 1);
-            this.buttons[1, 1] = (Button)this.FindViewById(Resource.Id.button11);
-            this.buttons[1, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 1], 1, 1);
-            this.buttons[2, 1] = (Button)this.FindViewById(Resource.Id.button21);
-            this.buttons[2, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 1], 2, 1);
+            //this.buttons[0, 1] = (Button)this.FindViewById(Resource.Id.button01);
+            //this.buttons[0, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 1], 0, 1);
+            //this.buttons[1, 1] = (Button)this.FindViewById(Resource.Id.button11);
+            //this.buttons[1, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 1], 1, 1);
+            //this.buttons[2, 1] = (Button)this.FindViewById(Resource.Id.button21);
+            //this.buttons[2, 1].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 1], 2, 1);
 
-            this.buttons[0, 2] = (Button)this.FindViewById(Resource.Id.button02);
-            this.buttons[0, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 2], 0, 2);
-            this.buttons[1, 2] = (Button)this.FindViewById(Resource.Id.button12);
-            this.buttons[1, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 2], 1, 2);
-            this.buttons[2, 2] = (Button)this.FindViewById(Resource.Id.button22);
-            this.buttons[2, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 2], 2, 2);
+            //this.buttons[0, 2] = (Button)this.FindViewById(Resource.Id.button02);
+            //this.buttons[0, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[0, 2], 0, 2);
+            //this.buttons[1, 2] = (Button)this.FindViewById(Resource.Id.button12);
+            //this.buttons[1, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[1, 2], 1, 2);
+            //this.buttons[2, 2] = (Button)this.FindViewById(Resource.Id.button22);
+            //this.buttons[2, 2].Click += (object? sender, EventArgs e) => this.ButtonClick(this.buttons[2, 2], 2, 2);
 
             for(int x = 0; x < 3; x++)
             {
